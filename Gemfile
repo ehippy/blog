81c4337f9c8fee5c0324571dbeecf7aff8de664c
Patrick McDavid <fusionjunky@gmail.com>
2025-12-19 21:27:56 -0700

    Revert "automatic javascript minification in the jekyll build"
    This reverts commit 8801fdf401d2d62d3d93ae954d5649b3f2d4b00b.

--- Gemfile
+++ Gemfile
@@ -1,15 +1,11 @@
 source 'https://rubygems.org'
 
-gem 'jekyll', '3.9.5'
-gem 'uglifier'
-gem 'jekyll-assets', '~> 3.0'
-gem 'sprockets', '~> 3.7'
-gem 'kramdown-parser-gfm'
+gem 'jekyll', '4.3.3'
 
 group :jekyll_plugins do
   gem 'jekyll-paginate'
   gem 'jekyll-sitemap'
   gem 'jekyll-feed'
-  gem 'jekyll-sass-converter', '~> 1.0'
+  gem 'jekyll-sass-converter', '>= 2.0'
   gem 'sassc'
 end
--- js/journal.js
+++ js/journal.js
@@ -8,16 +8,13 @@
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - Plugins
 
-//= require plugins/history
-//= require plugins/imagesloaded
-//= require plugins/masonry
-//= require plugins/debounce
-//= require plugins/fluidbox
-//= require plugins/owl
-//= require plugins/waypoints
-
-//= require dark-mode
-//= require archive-filter
+// @codekit-prepend "/plugins/history.js"
+// @codekit-prepend "/plugins/imagesloaded.js"
+// @codekit-prepend "/plugins/masonry.js"
+// @codekit-prepend "/plugins/debounce.js"
+// @codekit-prepend "/plugins/fluidbox.js"
+// @codekit-prepend "/plugins/owl.js"
+// @codekit-prepend "/plugins/waypoints.js"
 
 
 
